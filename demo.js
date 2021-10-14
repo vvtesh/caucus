aced5339f12abc4c14e730a9a4af4f4ddddf329a
vinayak <venkatesh.vinayakarao@here.com>
2021-10-14 09:00:31 +0530

    first demo

--- demo.js
+++ demo.js
@@ -25,6 +25,23 @@
   return obj;
 }
 
+function changeTime() {
+	     var radioButtons = document.getElementsByName("time");
+             for(var i = 0; i < radioButtons.length; i++)
+             {
+                 if(radioButtons[i].checked == true)
+                 {
+                     obj1.dispose();
+		 	if (radioButtons[i].value == 0) obj1 = addCircleToMap(map, 18.9225423, 72.8341395, 10, 'rgba(200, 0, 0, 0.1)');
+		 	if (radioButtons[i].value == 1) obj1 = addCircleToMap(map, 18.9225423, 72.8341395, 20, 'rgba(200, 0, 0, 0.2)');
+		 	if (radioButtons[i].value == 2) obj1 = addCircleToMap(map, 18.9225423, 72.8341395, 30, 'rgba(200, 0, 0, 0.4)');
+		 	if (radioButtons[i].value == 3) obj1 = addCircleToMap(map, 18.9225423, 72.8341395, 50, 'rgba(200, 0, 0, 0.6)');
+			map.addObject(obj1);
+                 }
+             }
+         
+}
+
 // Step 1: initialize communication with the platform
 // In your own code, replace variable window.apikey with your own apikey
 var platform = new H.service.Platform({
@@ -34,8 +51,8 @@ var defaultLayers = platform.createDefaultLayers();
 
 // Step 2: initialize a map
 var map = new H.Map(document.getElementById('map'), defaultLayers.vector.normal.map, {
-  zoom: 15,
-  center: { lat: 18.9067, lng: 72.8147 },
+  zoom: 16,
+  center: { lat: 18.9225423, lng: 72.8341395 },
   pixelRatio: window.devicePixelRatio || 1
 });
 
@@ -50,17 +67,8 @@ var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 // Step 4: create the default UI component, for displaying bubbles
 var ui = H.ui.UI.createDefault(map, defaultLayers);
 
-var obj1 = addCircleToMap(map, 18.9067, 72.8147, 80, 'rgba(0, 128, 0, 0.3)');
-var obj2 = addCircleToMap(map, 18.914042, 72.824693, 100, 'rgba(200, 0, 0, 0.3)');
-var obj3 = addCircleToMap(map, 18.914042, 72.818693, 100, 'rgba(200, 0, 0, 0.3)');
-var obj4 = addCircleToMap(map, 18.909, 72.817693, 100, 'rgba(200, 10, 0, 0.3)');
-var obj5 = addCircleToMap(map, 18.912042, 72.821093, 100, 'rgba(200, 0, 0, 0.3)');
+var obj1 = addCircleToMap(map, 18.9225423, 72.8341395, 50, 'rgba(200, 0, 0, 0.6)');
 
 map.addObject(obj1);
-map.addObject(obj2);
-map.addObject(obj3);
-map.addObject(obj4);
-map.addObject(obj5);
 
 //obj1.dispose();
-//obj2.dispose();
