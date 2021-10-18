153376f07a643e312c6cb2127646fdfc4fe11145
vinayak <venkatesh.vinayakarao@here.com>
2021-10-18 16:45:03 +0530

    changed bs marker to circle

--- demo-busstop.js
+++ demo-busstop.js
@@ -8,7 +8,8 @@
  * Boilerplate map initialization code starts below:
  */
 function clearAllObjects() {
-	currObj.dispose();
+	currObj1.dispose();
+	currObj2.dispose();
 }
 
 function changeTime() {
@@ -19,11 +20,17 @@ function changeTime() {
              {
                  if(radioButtons[i].checked == true)
                  {
-		 	if (radioButtons[i].value == 0) currObj = addCircleToMap(map, lat1, lng1 , 5, 'rgba(200, 0, 0, 1)');
-		 	if (radioButtons[i].value == 1) currObj = addCircleToMap(map, lat1, lng1 , 45, 'rgba(200, 0, 0, 0.45)');
-		 	if (radioButtons[i].value == 2) currObj = addCircleToMap(map, lat1, lng1 , 25, 'rgba(200, 0, 0, 0.25)');
-		 	if (radioButtons[i].value == 3) currObj = addCircleToMap(map, lat1, lng1 , 25, 'rgba(200, 0, 0, 0.25)');
-			map.addObject(currObj);
+                    if (radioButtons[i].value == 0) currObj1 = addCircleToMap(map, lat1, lng1 , 5, 'rgba(200, 0, 0, 1)');
+                    if (radioButtons[i].value == 1) currObj1 = addCircleToMap(map, lat1, lng1 , 45, 'rgba(200, 0, 0, 0.45)');
+                    if (radioButtons[i].value == 2) currObj1 = addCircleToMap(map, lat1, lng1 , 25, 'rgba(200, 0, 0, 0.25)');
+                    if (radioButtons[i].value == 3) currObj1 = addCircleToMap(map, lat1, lng1 , 25, 'rgba(200, 0, 0, 0.25)');
+                    map.addObject(currObj1);
+
+                    if (radioButtons[i].value == 0) currObj2 = addCircleToMap(map, lat2, lng2 , 5, 'rgba(200, 0, 0, 1)');
+                    if (radioButtons[i].value == 1) currObj2 = addCircleToMap(map, lat2, lng2 , 45, 'rgba(200, 0, 0, 0.45)');
+                    if (radioButtons[i].value == 2) currObj2 = addCircleToMap(map, lat2, lng2 , 25, 'rgba(200, 0, 0, 0.25)');
+                    if (radioButtons[i].value == 3) currObj2 = addCircleToMap(map, lat2, lng2 , 25, 'rgba(200, 0, 0, 0.25)');
+                    map.addObject(currObj2);
                  }
              }
 
@@ -49,17 +56,34 @@ function play() {
 	}
 }
 
-var currObj;
-var lat1 = 13.0613266;
-var lng1 = 80.2618077;
+var currObj1;
+var currObj2;
 
-var lat2 = 13.0607911;
-var lng2 = 80.2617750;
+var lat1 = 13.0216444;
+var lng1 = 80.0162790;
+
+var lat2 = 13.0223970;
+var lng2 = 80.0162147;
 
  function addCircleToMap(map, lat, lng, radius, color){
 
- var marker = new H.map.Marker({lat:lat, lng:lng});
- return marker;
+ //var marker = new H.map.Marker({lat:lat, lng:lng});
+ //return marker;
+ var obj =
+   new H.map.Circle(
+     // The central point of the circle
+     {lat: lat, lng: lng},
+     // The radius of the circle in meters
+     radius,
+     {
+       style: {
+         strokeColor: color, // Color of the perimeter
+         lineWidth: 2,
+         fillColor: color  // Color of the circle
+       }
+     }
+   );
+   return obj;
 
 }
 
@@ -69,11 +93,16 @@ function changeTime() {
              {
                  if(radioButtons[i].checked == true)
                  {
-                        currObj.dispose();
-		 	if (radioButtons[i].value % 2 == 0) currObj = addCircleToMap(map, lat1, lng1, 10, 'rgba(200, 0, 0, 0.1)');
-		 	if (radioButtons[i].value % 2 == 1) currObj = addCircleToMap(map, lat2, lng2, 20, 'rgba(200, 0, 0, 0.2)');
-
-			map.addObject(currObj);
+                        clearAllObjects();
+                        if (radioButtons[i].value % 2 == 0) {
+                            currObj1 = addCircleToMap(map, lat1, lng1, 40, 'rgba(200, 0, 0, 0.6)');
+                            currObj2 = addCircleToMap(map, lat2, lng2, 10, 'rgba(200, 0, 0, 0.6)');
+                        } else {
+                            currObj1 = addCircleToMap(map, lat1, lng1, 10, 'rgba(200, 0, 0, 0.6)');
+                            currObj2 = addCircleToMap(map, lat2, lng2, 40, 'rgba(200, 0, 0, 0.6)');
+                        }
+                        map.addObject(currObj1);
+                        map.addObject(currObj2);
                  }
              }
 
@@ -88,7 +117,7 @@ var defaultLayers = platform.createDefaultLayers();
 
 // Step 2: initialize a map
 var map = new H.Map(document.getElementById('map'), defaultLayers.vector.normal.map, {
-  zoom: 16,
+  zoom: 18,
   center: { lat: lat1, lng: lng1 },
   pixelRatio: window.devicePixelRatio || 1
 });
@@ -104,9 +133,11 @@ var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 // Step 4: create the default UI component, for displaying bubbles
 var ui = H.ui.UI.createDefault(map, defaultLayers);
 
-currObj = addCircleToMap(map, lat1, lng1, 50, 'rgba(200, 0, 0, 0.6)');
+currObj1 = addCircleToMap(map, lat1, lng1, 10, 'rgba(200, 0, 0, 0.6)');
+currObj2 = addCircleToMap(map, lat2, lng2, 40, 'rgba(200, 0, 0, 0.6)');
 
-map.addObject(currObj);
+map.addObject(currObj1);
+map.addObject(currObj2);
 
 //obj1.dispose();
 //check.
